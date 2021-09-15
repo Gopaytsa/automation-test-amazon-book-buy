45cfa49fb122e3fdde7a35b93d5a073d1bec599a
unknown <gopaytsa.olga@gmail.com>
2021-09-15 14:21:10 +0300

    run.py completed

---
+++ run.py
@@ -0,0 +1,38 @@
+"""
+Module for tests running with parameters described in README file.
+"""
+import argparse
+import sys
+import pytest
+
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument('--browser',
+                    help="Browser for tests run",
+                    default='chrome',
+                    choices=[
+                      'chrome',
+                      'firefox'
+                    ])
+
+parser.add_argument('--m',
+                    help='Marker to tests run',
+                    choices=[
+                      'amazon',
+                      'heroku'
+                      ])
+
+args = parser.parse_args()
+BROWSER = args.browser
+MARKER = args.m
+
+print(f'Tests are running in {BROWSER} browser')
+
+pytest_run = [f'--browser={BROWSER} -n3']\
+  
+if MARKER:
+  print(f'Tests running with {MARKER}')
+  pytest_run += ['-m', f'{MARKER}']
+  
+sys.exit(pytest.main(pytest_run))
