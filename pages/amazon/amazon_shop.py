c7ddc06f8b60287a9066dab934423c71828e3932
unknown <gopaytsa.olga@gmail.com>
2021-09-18 17:41:58 +0300

    started to add selectors

--- pages/amazon/amazon_shop.py
+++ pages/amazon/amazon_shop.py
@@ -1,15 +1,28 @@
-from selenium.webdriver.support.wait import WebDriverWait
-from selenium.webdriver.support import expected_conditions as ec 
 from selenium.webdriver.common.by import By 
-from selenium.common.exceptions import TimeoutException, NoSuchElementException
-from selenium.webdriver.common.action_chains import ActionChains
 
+from pages.web_elements.elements import Elements
 from pages.home_page import BasePage
+from pages.web_elements.locators import Locator
 
-
-class Locators:
-  pass
-
-
-class AmazonPage():
-  pass
+class AmazonPage(BasePage):
+  
+  @property
+  def department_dropdown(self):
+    locator = Locator(by=By.CSS_SELECTOR, value='#searchDropdownBox')
+    return Elements(self.driver, locator)
+  
+  @property
+  def book_department(self):
+    locator = Locator(by=By.CSS_SELECTOR, value='option:nth-child(6)')
+    return Elements(self.driver, locator)
+  
+  @property
+  def submit_searc_button(self):
+    locator = Locator(by=By.XPATH, value='//li/a')
+    for i in locator:
+      if i.text == "Children's Books":
+        pass
+  
+  @property
+  def select_item(self):
+    pass
--- pages/heroku/heroku_page.py
+++ pages/heroku/heroku_page.py
@@ -1,16 +1,8 @@
-from selenium.webdriver.support.wait import WebDriverWait
-from selenium.webdriver.support import expected_conditions as ec 
 from selenium.webdriver.common.by import By 
-from selenium.common.exceptions import TimeoutException, NoSuchElementException
-from selenium.webdriver.common.action_chains import ActionChains
 
+from pages.web_elements import Elements
 from pages.home_page import BasePage
+from pages.web_elements import Elements
 
-
-
-class Locators:
-  pass
-
-
-class HerokuPage():
+class HerokuPage(BasePage):
   pass
